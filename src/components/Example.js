82c8ad64e9cc2dcb6a94331bccebb3d3b72fcc54
Sumit Singh <sumit.sssingh.singh@gmail.com>
2020-10-01 18:36:22 +0530

    Dropdown change achieved

--- src/components/Example.js
+++ src/components/Example.js
@@ -3,6 +3,7 @@ import { Button, StyleSheet, Text, View } from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
 
 import FontTypes from '../enums/FontTypes';
+import DropDownPicker from 'react-native-dropdown-picker';
 
 const Example = props => {
     const [fontFamily, setFontFamily] = useState('SansitaSwashed-Bold');
@@ -10,14 +11,28 @@ const Example = props => {
         fontSize: 24,
         fontFamily: fontFamily
     }
+    const fontNames = [Object.keys(FontTypes)];
     return (
         <View style={styles.container}>
             <Text>Open up App.js to start working on your app!</Text>
             <Text style={fontStyle}>This is the header</Text>
             <TextInput style={styles.input} placeholder='Enter some text' />
-            <Button title='Click here to change header style' onPress={() => {
-                setFontFamily('SansitaSwashed-Black');
-            }} />
+
+            <DropDownPicker style={{ width: '50%' }}
+                items={[
+                    { label: 'Font 1', value: FontTypes.SansitaSwashedBlack },
+                    { label: 'Font 2', value: FontTypes.SansitaSwashedBold },
+                ]}
+                defaultIndex={0}
+                placeholder="Select a Font Style"
+                containerStyle={{ height: 40 }}
+                dropDownStyle={{ width: 120 }}
+                onChangeItem={item => setFontFamily(item.value)}
+            />
+
+            <View style={StyleSheet.hairlineWidth} />
+
+
         </View>
     );
 };
