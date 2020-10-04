57098d6b9e76ca877a1e1615f694cf05a7ed80ed
Sumit Singh <sumit.sssingh.singh@gmail.com>
2020-10-04 11:40:36 +0530

    error works

--- src/components/Example.js
+++ src/components/Example.js
@@ -8,27 +8,36 @@ import DropDownPicker from 'react-native-dropdown-picker';
 const Example = props => {
     const [fontFamily, setFontFamily] = useState('SansitaSwashed-Bold');
     const fontStyle = {
-        fontSize: 24,
-        fontFamily: fontFamily
+        fontSize: 16,
+        fontFamily: fontFamily,
+        width: '50%'
     }
     const fontNames = [Object.keys(FontTypes)];
     return (
         <View style={styles.container}>
             <Text>Open up App.js to start working on your app!</Text>
-            <Text style={fontStyle}>This is the header</Text>
-            <TextInput style={styles.input} placeholder='Enter some text' />
+            <Text style={styles.input}>This is the header</Text>
+            <View style={{ flexDirection: "row" }}>
+                <TextInput style={fontStyle} placeholder='Enter some text' />
 
-            <DropDownPicker style={{ width: '50%' }}
-                items={[
-                    { label: 'Font 1', value: FontTypes.SansitaSwashedBlack },
-                    { label: 'Font 2', value: FontTypes.SansitaSwashedBold },
-                ]}
-                defaultIndex={0}
-                placeholder="Select a Font Style"
-                containerStyle={{ height: 40 }}
-                dropDownStyle={{ width: 120 }}
-                onChangeItem={item => setFontFamily(item.value)}
-            />
+                <DropDownPicker style={{ width: '100%' }}
+                    items={[
+                        { label: 'SansitaBlack', value: FontTypes.SansitaSwashedBlack },
+                        { label: 'SansitsBold', value: FontTypes.SansitaSwashedBold },
+                        { label: 'SansitaExtraBold', value: FontTypes.SansitaSwashedExtraBold },
+                        { label: 'SansitaLight', value: FontTypes.SansitaSwashedLight },
+                    ]}
+                    defaultIndex={0}
+                    placeholder="Select a Font Style"
+                    containerStyle={{ height: 40 }}
+                    dropDownStyle={{ width: 160 }}
+                    onChangeItem={item => {
+                        setFontFamily(item.value);
+                        console.log('Current style is ', fontFamily);
+                    }}
+                />
+            </View>
+
 
             <View style={StyleSheet.hairlineWidth} />
 
@@ -48,7 +57,7 @@ const styles = StyleSheet.create({
 
     },
     input: {
-        fontSize: 16,
+        fontSize: 24,
         fontFamily: 'SansitaSwashed-Regular'
     }
 });
